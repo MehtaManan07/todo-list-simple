4d2f25fb820e60b79e545c8b04a253cbc2a4e5ff
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-05-25 11:54:36 +0530

    Updates

---
+++ precache-manifest.1f79bca6f22bccfe1176b9fcd1ecf398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b465b4ae568c0aadb3f201356650aa",
+    "url": "/todo-list-simple/index.html"
+  },
+  {
+    "revision": "9398acd8913f7758cb95",
+    "url": "/todo-list-simple/static/css/main.f1d6e3d3.chunk.css"
+  },
+  {
+    "revision": "95a18a69d0122dcf2717",
+    "url": "/todo-list-simple/static/js/2.c3779657.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-simple/static/js/2.c3779657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9398acd8913f7758cb95",
+    "url": "/todo-list-simple/static/js/main.eb5ce51a.chunk.js"
+  },
+  {
+    "revision": "40b470bc9f141141ef8b",
+    "url": "/todo-list-simple/static/js/runtime-main.33d26f70.js"
+  }
+]);
